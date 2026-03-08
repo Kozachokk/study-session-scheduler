92ed095ff4d2144fe1877d78dc7de48fa8585e63
Yaroslav K <yarokl100@gmail.com>
2026-03-08 11:59:23 -0400

    share

--- frontend/app/login/components/LoginBox.tsx
+++ frontend/app/login/components/LoginBox.tsx
@@ -1,5 +1,14 @@
 function LoginBox(){
-    return(<div>Modal</div>)
+    return(
+    <div className="flex flex-col border-dark3 border-4 pt-8">
+        <h2>Log In<br/>Signup</h2>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' name='username' id='username' className="border-2 rounded-md pl-2 selection::bg-transparent mb-2 w-[25vw] ml-[2.5vw] mr-[2.5vw]"></input>
+        <label htmlFor='passwd'>Password:</label>
+        <input type='password' name='passwd' id='passwd' className="border-2 rounded-md pl-2 selection::bg-transparent mb-2 w-[25vw] ml-[2.5vw] mr-[2.5vw]"></input>
+        <button type='submit'></button>
+    </div>
+)
 }
 
 export default LoginBox;
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginBox from "./components/LoginBox";
 
 export default function Home() {
   return (
-      <main className="bg-dark1 text-light3 h-full overflow-hidden ">
+      <main className="bg-dark1 text-light3 h-full overflow-hidden flex justify-center items-center">
         <LoginBox />
       </main>
   );
